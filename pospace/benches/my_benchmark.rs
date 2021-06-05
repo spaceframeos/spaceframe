5ed2b03172b6eab735dad9803dbcad8a42578324
Gil Balsiger <gil.balsiger@gmail.com>
2021-06-05 12:37:55 +0200

    perf: add phase1 benchmark

--- pospace/benches/my_benchmark.rs
+++ pospace/benches/my_benchmark.rs
@@ -1,6 +1,11 @@
 use bitvec::{bitvec, order::Lsb0};
-use criterion::{BenchmarkId, Criterion, black_box, criterion_group, criterion_main};
-use spaceframe_pospace::{bits::{BitsWrapper, from_bits, to_bits}, core::PoSpace, fx_calculator::{calculate_blake_hash, FXCalculator}};
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use rand::{RngCore, rngs::OsRng};
+use spaceframe_pospace::{
+    bits::{from_bits, to_bits, BitsWrapper},
+    core::PoSpace,
+    fx_calculator::{calculate_blake_hash, FXCalculator},
+};
 
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("blake3", |b| {
@@ -33,20 +38,6 @@ fn criterion_benchmark(c: &mut Criterion) {
         })
     });
 
-    c.bench_function("to_bits", |b| {
-        b.iter(|| {
-            to_bits(black_box(0xabcd), black_box(16));
-        })
-    });
-
-    let input = bitvec![Lsb0, u8; 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1];
-
-    c.bench_with_input(BenchmarkId::new("from_bits", &input), &input, |b, vec| {
-        b.iter(|| {
-            from_bits(vec);
-        })
-    });
-
     c.bench_function("matching", |b| {
         let pospace = PoSpace::new(14, b"some key");
         let l = BitsWrapper::new(bitvec![Lsb0, u8; 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1]);
@@ -55,7 +46,35 @@ fn criterion_benchmark(c: &mut Criterion) {
             pospace.matching(&l, &r);
         })
     });
+
+    let mut bits_group = c.benchmark_group("bits");
+    let input = bitvec![Lsb0, u8; 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1];
+    bits_group.bench_function("to_bits", |b| {
+        b.iter(|| {
+            to_bits(black_box(0xabcd), black_box(16));
+        })
+    });
+    bits_group.bench_function("from_bits", |b| {
+        b.iter(|| {
+            from_bits(black_box(&input));
+        })
+    });
+}
+
+fn phase1_benchmark(c: &mut Criterion) {
+    let mut plot_seed = [0u8; 32];
+    OsRng.fill_bytes(&mut plot_seed);
+    let pos = PoSpace::new(10, &plot_seed);
+
+    c.bench_function("run_phase1", |b| {
+        b.iter(|| pos.run_phase_1());
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
-criterion_main!(benches);
+criterion_group!{
+    name = phase1;
+    config = Criterion::default().sample_size(50).significance_level(0.1).noise_threshold(0.05);
+    targets = phase1_benchmark
+}
+criterion_main!(benches, phase1);
