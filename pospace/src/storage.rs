f37e80ed28fae0496f41a1980ac140ee617c3d36
Gil Balsiger <gil.balsiger@gmail.com>
2021-06-28 15:04:21 +0200

    feat: find_min_chunk function

--- pospace/src/storage.rs
+++ pospace/src/storage.rs
@@ -1,9 +1,9 @@
 use glob::glob;
 use std::{
+    collections::VecDeque,
     fs::File,
     io::{Read, Write},
-    os::unix::prelude::MetadataExt,
-    path::Path,
+    path::{Path, PathBuf},
 };
 
 use crate::bits::BitsWrapper;
@@ -90,39 +90,10 @@ pub fn sort_table1() {
 
     // K-Way Merge sort
     println!("K-Way merging for table 1 ...");
+    let mut state = KWayMergeState::new(&parts, serialized_size);
 
     // Load BUCKET_SIZE / (chunks_count - 1) buckets into RAM
     if chunks_count > 1 {
-        let merge_chunck_size = BUCKET_SIZE / (chunks_count - 1);
-        let mut state = KWayMergeState {
-            chunks: Vec::new(),
-            output: Vec::new(),
-        };
-
-        for path in parts {
-            let mut file = File::open(path).unwrap();
-            let mut buffer = vec![0u8; serialized_size * merge_chunck_size];
-            let amount = file.read(&mut buffer).unwrap();
-            println!("Red {} bytes", amount);
-            let entries = buffer
-                .chunks(serialized_size as usize)
-                .filter_map(|chunk| {
-                    if chunk.iter().all(|c| c == &0u8) {
-                        return None;
-                    } else {
-                        return Some(bincode::deserialize(&chunk).unwrap());
-                    }
-                })
-                .collect::<Vec<Table1Entry>>();
-            let total_size = file.metadata().unwrap().len() as usize / serialized_size;
-            state.chunks.push(MergeChunk {
-                remaining: total_size - entries.len(),
-                content: entries,
-                indice: 0,
-                total_size,
-            });
-        }
-
         println!(
             "{:?}",
             state
@@ -151,9 +122,60 @@ struct KWayMergeState {
     output: Vec<Table1Entry>,
 }
 
+impl KWayMergeState {
+    pub fn new(paths: &[PathBuf], entry_size: usize) -> Self {
+        let merge_chunck_size = BUCKET_SIZE / (paths.len() - 1);
+        let mut state = KWayMergeState {
+            chunks: Vec::new(),
+            output: Vec::new(),
+        };
+
+        for path in paths {
+            let mut file = File::open(path).unwrap();
+            let mut buffer = vec![0u8; entry_size * merge_chunck_size];
+            let amount = file.read(&mut buffer).unwrap();
+            println!("Red {} bytes", amount);
+            let entries = buffer
+                .chunks(entry_size as usize)
+                .filter_map(|chunk| {
+                    if chunk.iter().all(|c| c == &0u8) {
+                        return None;
+                    } else {
+                        return Some(bincode::deserialize(&chunk).unwrap());
+                    }
+                })
+                .collect::<VecDeque<Table1Entry>>();
+            let total_size = file.metadata().unwrap().len() as usize / entry_size;
+            state.chunks.push(MergeChunk {
+                remaining: total_size - entries.len(),
+                content: entries,
+                indice: 0,
+                total_size,
+                file_path: path.clone().into_os_string().into_string().unwrap(),
+            });
+        }
+        state
+    }
+
+    pub fn run_iteration(&mut self) -> Result<(), ()> {}
+
+    pub fn find_min_chunk(&self) -> usize {
+        self.chunks
+            .iter()
+            .map(|c| c.content[c.indice])
+            .collect::<Vec<Table1Entry>>()
+            .iter()
+            .enumerate()
+            .min_by_key(|(_, x)| x)
+            .unwrap()
+            .0
+    }
+}
+
 #[derive(Debug)]
 struct MergeChunk {
-    content: Vec<Table1Entry>,
+    file_path: String,
+    content: VecDeque<Table1Entry>,
     indice: usize,
     total_size: usize,
     remaining: usize,
