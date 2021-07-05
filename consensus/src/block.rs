1603aa1876b072a594268dfb4944d095e3339f14
Gil Balsiger <gil.balsiger@gmail.com>
2021-07-05 09:16:55 +0200

    feat(consensus): initial data structures

---
+++ consensus/src/block.rs
@@ -0,0 +1,25 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Block {
+    height: u64,
+    hash: Vec<u8>,
+    previous_hash: Vec<u8>,
+    transactions: Vec<Transaction>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Transaction {
+    hash: Vec<u8>,
+    signature: Vec<u8>,
+    timestamp: u64,
+    inputs: Vec<TransactionIO>,
+    outputs: Vec<TransactionIO>,
+    fee: f64,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct TransactionIO {
+    address: Vec<u8>,
+    amount: f64,
+}
---
+++ consensus/src/hash.rs
@@ -0,0 +1,9 @@
+use crate::block::{Block, Transaction};
+
+pub fn calculate_block_hash(block: &Block) -> Vec<u8> {
+    todo!();
+}
+
+pub fn calculate_transaction_hash(transaction: &Transaction) -> Vec<u8> {
+    todo!()
+}
