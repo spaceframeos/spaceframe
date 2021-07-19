5f0fe5b6ae9946475edc747efdc824136c2bc8cc
Gil Balsiger <gil.balsiger@gmail.com>
2021-07-19 18:28:58 +0200

    feat(powork): basic proof of work implementation

--- ledger/src/proof.rs
+++ ledger/src/proof.rs
@@ -1,14 +1,15 @@
 use borsh::{BorshDeserialize, BorshSerialize};
 use std::fmt::Debug;
 
-pub trait Proof: BorshSerialize + BorshDeserialize + Clone + PartialEq + Debug {}
-
-#[derive(BorshSerialize, BorshDeserialize, Clone, PartialEq, Debug)]
-pub struct PoWorkProof {}
-
-impl Proof for PoWorkProof {}
+pub trait Proof: BorshSerialize + BorshDeserialize + Clone + PartialEq + Debug {
+    fn find_proof<T: AsRef<[u8]>>(data: T) -> Self;
+}
 
 #[derive(BorshSerialize, BorshDeserialize, Clone, PartialEq, Debug)]
 pub struct PoSpaceProof {}
 
-impl Proof for PoSpaceProof {}
+impl Proof for PoSpaceProof {
+    fn find_proof<T: AsRef<[u8]>>(_data: T) -> Self {
+        todo!()
+    }
+}
--- powork/src/lib.rs
+++ powork/src/lib.rs
@@ -1,1 +1,45 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use digest::Digest;
+use sha2::Sha256;
+use spaceframe_ledger::proof::Proof;
+use std::fmt::Debug;
 
+#[derive(BorshSerialize, BorshDeserialize, Clone, PartialEq, Debug)]
+pub struct PoWorkProof<const N: u8> {
+    nonce: u64,
+}
+
+impl<const N: u8> Proof for PoWorkProof<N> {
+    fn find_proof<T: AsRef<[u8]>>(data: T) -> Self {
+        let difficulty = N;
+        let diff_str = (0..difficulty).map(|_| "0").collect::<String>();
+
+        let mut proof: u64 = 0u64;
+
+        loop {
+            let mut bytes = proof.to_le_bytes().to_vec();
+            bytes.extend_from_slice(data.as_ref());
+            let hash = hex::encode(Sha256::digest(&bytes));
+            if hash.starts_with(&diff_str) {
+                return Self { nonce: proof };
+            }
+            proof += 1;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use sha2::Sha256;
+
+    #[test]
+    fn test_powork() {
+        let data = b"coucou";
+        let pow = PoWorkProof::<3>::find_proof(&data);
+        let mut bytes = pow.nonce.to_le_bytes().to_vec();
+        bytes.extend_from_slice(data.as_ref());
+        let hash = Sha256::digest(&bytes).to_vec();
+        assert!(hex::encode(&hash).starts_with("000"));
+    }
+}
